41139d4aaf9270c8df509b41d0da0a3570800c29
chinponya <czen+github@honk.li>
2026-03-20 21:36:28 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs
+      ];
+    };
+  };
+}
